73ac0799c1f3b90dfb402aae0a1a88ae4aa6b259
deksuperbagus-collab <deksuperbagus@gmail.com>
2025-12-24 22:13:09 +0800

    Add files via upload

---
+++ storage/framework/views/e046cd8e1e79283a40359727f610c712.php
@@ -0,0 +1,12 @@
+<?php $__env->startSection('page','Welcome Page'); ?>
+
+<?php $__env->startSection('konten'); ?>
+<div class="container">
+
+    <div class="konten">
+        <h3>Welcome page</h3>
+    <div>
+
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\INSTIKI\Documents\perpustakaan\resources\views/welcome.blade.php ENDPATH**/ ?>
